2817ebf160f51f7b6443fc73ba59c04aec152086
bonnina <37539513+bonnina@users.noreply.github.com>
2019-01-20 16:34:41 +0200

    Add pages for CRUD operations  for the student model

---
+++ CollegeApp/Data/SchoolContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace CollegeApp.Models
+{
+    public class SchoolContext : DbContext
+    {
+        public SchoolContext (DbContextOptions<SchoolContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CollegeApp.Models.Student> Student { get; set; }
+    }
+}
