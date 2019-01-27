d4742539fb38a46fd53d429311f55bba169ac704
bonnina <37539513+bonnina@users.noreply.github.com>
2019-01-27 14:54:10 +0200

    Update Create

--- CollegeApp/Pages/Students/Create.cshtml.cs
+++ CollegeApp/Pages/Students/Create.cshtml.cs
@@ -32,11 +32,19 @@ namespace CollegeApp.Pages.Students
             {
                 return Page();
             }
+            var emptyStudent = new Student();
 
-            _context.Student.Add(Student);
-            await _context.SaveChangesAsync();
+            if (await TryUpdateModelAsync<Student>(
+                emptyStudent,
+                "student",   // Prefix for form value.
+                s => s.FirstMidName, s => s.LastName, s => s.EnrollmentDate))
+            {
+                _context.Student.Add(emptyStudent);
+                await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
+            }
 
-            return RedirectToPage("./Index");
+            return null;
         }
     }
 }
