61fb21a26303c3b363cc0e2177b4c9d426041c89
bonnina <37539513+bonnina@users.noreply.github.com>
2019-01-30 07:53:40 +0200

    Add filtering by name

--- CollegeApp/Pages/Students/Index.cshtml.cs
+++ CollegeApp/Pages/Students/Index.cshtml.cs
@@ -24,13 +24,20 @@ namespace CollegeApp.Pages.Students
         public string CurrentSort { get; set; }
         public IList<Student> Student { get;set; }
 
-        public async Task OnGetAsync(string sortOrder)
+        public async Task OnGetAsync(string sortOrder, string searchString)
         {
             NameSort = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
             DateSort = sortOrder == "Date" ? "date_desc" : "Date";
+            CurrentFilter = searchString;
 
             IQueryable<Student> studentIQ = from s in _context.Student
                                             select s;
+
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                studentIQ = studentIQ.Where(s => s.LastName.Contains(searchString)
+                                       || s.FirstMidName.Contains(searchString));
+            }
 
             switch (sortOrder)
             {
