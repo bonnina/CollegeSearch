859ef88c2bfce80b231d63a9577ed51da5225609
bonnina <37539513+bonnina@users.noreply.github.com>
2019-02-07 22:36:18 +0200

    Add StringLength and Column attr-s

--- CollegeApp/Models/Student.cs
+++ CollegeApp/Models/Student.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CollegeApp.Models
 {
     public class Student
     {
         public int ID { get; set; }
+        [StringLength(50, ErrorMessage = "Last name cannot be longer than 30 characters.")]
         public string LastName { get; set; }
+        [StringLength(50, ErrorMessage = "First name cannot be longer than 30 characters.")]
+        [Column("FirstName")]
         public string FirstMidName { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
