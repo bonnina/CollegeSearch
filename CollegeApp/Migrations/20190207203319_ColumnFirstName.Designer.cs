363209bbec975fb1d455f2eed424007eda4fb24e
bonnina <37539513+bonnina@users.noreply.github.com>
2019-02-07 22:38:03 +0200

    Column FirstName Migration

--- CollegeApp/Migrations/20190207203319_ColumnFirstName.Designer.cs
+++ CollegeApp/Migrations/20190207203319_ColumnFirstName.Designer.cs
@@ -4,14 +4,16 @@ using CollegeApp.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CollegeApp.Migrations
 {
     [DbContext(typeof(SchoolContext))]
-    partial class SchoolContextModelSnapshot : ModelSnapshot
+    [Migration("20190207203319_ColumnFirstName")]
+    partial class ColumnFirstName
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
