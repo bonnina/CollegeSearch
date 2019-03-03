ed60c2a4abb1b41163a6599fb008eab859915778
bonnina <37539513+bonnina@users.noreply.github.com>
2019-03-03 23:23:34 +0200

    Update instructor Edit page model

--- CollegeApp/Pages/Instructors/Edit.cshtml.cs
+++ CollegeApp/Pages/Instructors/Edit.cshtml.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using CollegeApp.Models;
 
 namespace CollegeApp.Pages.Instructors
 {
-    public class EditModel : PageModel
+    public class EditModel : InstructorCoursesPageModel
     {
         private readonly CollegeApp.Models.SchoolContext _context;
 
@@ -31,6 +29,7 @@ namespace CollegeApp.Pages.Instructors
 
             Instructor = await _context.Instructors
                 .Include(i => i.OfficeAssignment)
+                .Include(i => i.CourseAssignments).ThenInclude(i => i.Course)
                 .AsNoTracking()
                 .FirstOrDefaultAsync(m => m.ID == id);
 
@@ -38,10 +37,11 @@ namespace CollegeApp.Pages.Instructors
             {
                 return NotFound();
             }
+            PopulateAssignedCourseData(_context, Instructor);
             return Page();
         }
 
-        public async Task<IActionResult> OnPostAsync(int? id)
+        public async Task<IActionResult> OnPostAsync(int? id, string[] selectedCourses)
         {
             if (!ModelState.IsValid)
             {
@@ -50,6 +50,8 @@ namespace CollegeApp.Pages.Instructors
 
             var instructorToUpdate = await _context.Instructors
              .Include(i => i.OfficeAssignment)
+             .Include(i => i.CourseAssignments)
+                 .ThenInclude(i => i.Course)
              .FirstOrDefaultAsync(s => s.ID == id);
 
             if (await TryUpdateModelAsync<Instructor>(
@@ -63,9 +65,14 @@ namespace CollegeApp.Pages.Instructors
                 {
                     instructorToUpdate.OfficeAssignment = null;
                 }
+                UpdateInstructorCourses(_context, selectedCourses, instructorToUpdate);
                 await _context.SaveChangesAsync();
+                return RedirectToPage("./Index");
             }
-            return RedirectToPage("./Index");
+
+            UpdateInstructorCourses(_context, selectedCourses, instructorToUpdate);
+            PopulateAssignedCourseData(_context, instructorToUpdate);
+            return Page();
         }
     }
 }
