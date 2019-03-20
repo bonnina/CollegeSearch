d6166d19e0871fb160569b4454432d475d42a1f8
bonnina <37539513+bonnina@users.noreply.github.com>
2019-03-20 07:56:36 +0200

    Update Student model to inherit common fields from Person

--- CollegeApp/Models/Student.cs
+++ CollegeApp/Models/Student.cs
@@ -5,31 +5,13 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace CollegeApp.Models
 {
-    public class Student
+    public class Student : Person
     {
-        public int ID { get; set; }
-        [Required]
-        [StringLength(30, ErrorMessage = "Last name cannot be longer than 30 characters.")]
-        [Display(Name = "Last Name")]
-        public string LastName { get; set; }
-        [StringLength(30, ErrorMessage = "First name cannot be longer than 30 characters.")]
-        [Column("FirstName")]
-        [Display(Name = "First Name")]
-        public string FirstMidName { get; set; }
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [Display(Name = "Enrollment Date")]
         public DateTime EnrollmentDate { get; set; }
-        [Display(Name = "Full Name")]
-        public string FullName
-        {
-            get
-            {
-                return LastName + ", " + FirstMidName;
-            }
-        }
-
-
+       
         public ICollection<Enrollment> Enrollments { get; set; }
     }
 }
